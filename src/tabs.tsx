13e5b7f9eba845879212d449c87f79b6421a3611
Eric Zhang <ericbkzhang@gmail.com>
2025-06-08 09:26:17 -0400

    Move all files from `/vite-js` to `.`

--- src/tabs.tsx
+++ src/tabs.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
+import type { MouseEvent } from "react";
 
 function Tabs() {
     const [currentTab, setCurrentTab] = useState("home-tab");
 
-    function handleClick(event) {
-        setCurrentTab(event.target.id);
+    function handleClick(event: MouseEvent<HTMLDivElement>) {
+        setCurrentTab(event.currentTarget.id);
         console.log(`Current tab: ${currentTab}`);
     }
 
     return (
         <>
-            <div className="bg-gray-800 text-gray-50 w-full h-2/5" id="tabs" onClick={handleClick}>
+            <div
+                className="bg-gray-800 text-gray-50 w-full h-2/5"
+                id="tabs"
+                onClick={handleClick}
+            >
                 <div id="home-tab">Home</div>
                 <div id="about-us-tab">About Us</div>
             </div>
