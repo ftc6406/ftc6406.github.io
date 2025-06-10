2fa8ef60660c34cdd780ea057837f86602aa7246
Eric Zhang <ericbkzhang@gmail.com>
2025-06-10 11:16:30 -0400

    Right align header tabs

--- src/tabs.tsx
+++ src/tabs.tsx
@@ -14,14 +14,14 @@ function Tabs() {
     return (
         <>
             <div
-                className="w-full h-1/3 bg-gray-700 text-gray-50 text-xl content-stretch flex"
+                className="w-full h-1/3 bg-gray-700 text-gray-50 text-xl justify-end flex"
                 id="tabs"
                 onClick={handleClick}
             >
                 {TAB_NAMES.map((tabName, index) => (
                     <div
                         key={index}
-                        className="tab px-50 py-4 hover:bg-gray-600"
+                        className="tab px-25 py-4 hover:bg-gray-600"
                         id={`${tabName.toLowerCase().replace(" ", "-")}-tab`}
                     >
                         {tabName}
