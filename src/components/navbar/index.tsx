bc784fa1afb1b4cc9d948012cfbc9af99048578b
Eric Zhang <ericbkzhang@gmail.com>
2025-06-16 23:29:19 -0400

    Merge logo and tabs into Navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import type { MouseEvent } from "react";
 
-function Tabs({height}: {height: number}) {
+function Navbar() {
     const [currentTab, setCurrentTab] = useState("home-tab");
 
     function handleClick(event: MouseEvent<HTMLDivElement>) {
@@ -10,25 +10,27 @@ function Tabs({height}: {height: number}) {
     }
 
     const TAB_NAMES: string[] = ["Home", "About Us"];
+
     return (
-        <>
-            <div
-                className={`w-full h-${height} p-${height} bg-zinc-800 text-xl justify-end flex`}
-                id="tabs"
-                onClick={handleClick}
-            >
+        <div className="w-full bg-zinc-800 text-xl flex justify-between items-center p-4">
+            <div>
+                <img src="assets/logo.png" alt="Logo" className="size-10" />
+            </div>
+
+            <div className="flex justify-end">
                 {TAB_NAMES.map((tabName, index) => (
-                    <div
+                    <p
                         key={index}
-                        className="tab px-[5%] py-4 hover:bg-zinc-700 align-middle"
+                        className="tab px-[5%] hover:bg-zinc-700 align-middle text-nowrap"
                         id={`${tabName.toLowerCase().replace(" ", "-")}-tab`}
+                        onClick={handleClick}
                     >
                         {tabName}
-                    </div>
+                    </p>
                 ))}
             </div>
-        </>
+        </div>
     );
 }
 
-export default Tabs;
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
-import App from "./App.tsx";
-import Tabs from "./tabs.tsx";
-import Logo from "./logo.tsx";
+import Navbar from "./components/navbar";
 
-const LOGO_HEIGHT: number = 16;
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <Logo height={LOGO_HEIGHT} />
-        <Tabs height={LOGO_HEIGHT} />
-        <App />
+        <Navbar />
     </StrictMode>
 );
 
