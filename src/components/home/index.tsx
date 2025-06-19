e391f840380f73dc872873560e58d00043d23ef2
Eric Zhang <ericbkzhang@gmail.com>
2025-06-18 22:55:16 -0400

    Create components for each tab

---
+++ src/components/home/index.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="flex justify-center items-center">
+            <img className="opacity-50" src="/assets/man_mountain.jpg"/>
+        </div>
+    )
+}
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import Navbar from "./components/navbar";
-import Image from "./components/image";
+import Home from "./components/home";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Navbar />
-        <Image />
+        <Home />
     </StrictMode>
 );
