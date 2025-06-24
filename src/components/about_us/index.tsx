974826bd936246fe0e3c833cf8af55e8c55c023b
Eric Zhang <ericbkzhang@gmail.com>
2025-06-24 19:38:30 -0400

    Add rounded rectangle backgrounds to pages. Center titles

--- src/components/about_us/index.tsx
+++ src/components/about_us/index.tsx
@@ -1,12 +1,12 @@
 function AboutUs() {
     return (
-        <div id="about-us" className="m-8">
-            <h1 className="text-4xl">About Us</h1>
+        <div id="about-us" className="m-8 p-8 rounded-xl bg-zinc-800">
+            <h1 className="text-4xl text-center">About Us</h1>
             <p>
                 Hello, this is some placeholder text for the About Us section.
             </p>
 
-            <h2 className="text-3xl mt-4">Awards</h2>
+            <h2 className="text-3xl mt-8 text-center">Awards</h2>
             <ul>
                 <li>2022 - Judges' Choice Award</li>
                 <li>2023 - Design Award</li>
--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Navbar />
         <Home />
-        <AboutUs /> 
+        <AboutUs />
         <Gallery />
         <ContactUs />
     </StrictMode>
