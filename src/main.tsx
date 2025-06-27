49b73237cadb7e326d5ca28ddbc5ceb68168ebcb
Eric Zhang <ericbkzhang@gmail.com>
2025-06-27 00:00:15 -0400

    Correct a call to `Pages`

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Navbar from "./components/navbar";
 import Home from "./components/home";
-import Pages from "./components/non_main_pages";
+import NonMainPages from "./components/non_main_pages";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Navbar />
         <Home />
-        <Pages />
+        <NonMainPages />
     </StrictMode>
 );
