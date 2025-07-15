622fa6a765223e2cef00e13a00c200e9fafd140f
Eric Zhang <ericbkzhang@gmail.com>
2025-07-15 11:17:47 -0400

    Redesign navbar

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,9 @@
+function Hero() {
+    return (
+        <div className="flex justify-center items-center bg-black" id="home">
+            <img className="opacity-70" src="/assets/man_mountain.jpg" />
+        </div>
+    );
+}
+
+export default Hero;
