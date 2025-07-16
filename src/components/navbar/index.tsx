170c463c3bfb5896aade221d721e75c27d953abc
Eric Zhang <ericbkzhang@gmail.com>
2025-07-16 14:03:23 -0400

    Move logo in navbar to a separate file

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,20 +1,11 @@
+import Logo from "./logo";
+
 function Navbar() {
     const TAB_NAMES = ["Gallery", "Awards", "Join Us"];
 
     return (
         <div className="fixed w-full z-100 px-8 flex justify-between">
-            <a href="#hero">
-                <div className="flex sm:p-4 justify-self-start items-center">
-                    <img
-                        src="assets/logo.svg"
-                        alt="Logo"
-                        className="md:w-14 sm:w-4"
-                    />
-                    <div className="ml-4 md:ml-8 text-2xl md:text-4xl font-bold">
-                        First Tech Coyotes
-                    </div>
-                </div>
-            </a>
+            <Logo></Logo>
 
             <div className="flex justify-end space-x-2 md:space-x-16">
                 {TAB_NAMES.map((tabName, index) => {
