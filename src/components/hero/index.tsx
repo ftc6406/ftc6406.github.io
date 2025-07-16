66ef503aa8a400a5cd8dc5d328ed1fd7c4b20732
Eric Zhang <ericbkzhang@gmail.com>
2025-07-16 19:52:47 -0400

    Reformat logo and splash text on hero

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,20 +1,16 @@
 function Hero() {
     return (
         <div className="flex justify-center items-center" id="hero">
-            <img
-                className="bg-black opacity-75"
-                src="/assets/man_mountain.jpg"
-            />
+            <img className="brightness-75" src="/assets/man_mountain.jpg" />
 
-            <img
-                src="/assets/logo.svg"
-                className="absolute -ml-280 -mt-80"
-            ></img>
+            <div className="absolute flex">
+                <img src="/assets/logo.svg" className=""/>
 
-            <p className="absolute -ml-50 -mt-100 text-[80px] font-bold">
-                First Tech Coyotes
-            </p>
-            <p className="absolute -ml-[406.61px] -mt-55 text-6xl">Student engineers</p>
+                <div className="">
+                    <p className="text-[80px] font-bold">First Tech Coyotes</p>
+                    <p className="text-6xl">Student engineers</p>
+                </div>
+            </div>
         </div>
     );
 }
