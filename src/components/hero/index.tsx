2dbf8614e0919678b2047b0b916f806f24d92d9d
Eric Zhang <ericbkzhang@gmail.com>
2025-07-16 20:07:51 -0400

    Reformat hero page

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -2,19 +2,17 @@ function Hero() {
     return (
         <div className="flex justify-center items-center" id="hero">
             <img
-                className="bg-black opacity-75"
+                className="-z-10 w-screen brightness-75"
                 src="/assets/man_mountain.jpg"
             />
 
-            <img
-                src="/assets/logo.svg"
-                className="absolute -ml-280 -mt-80"
-            ></img>
-
-            <p className="absolute -ml-50 -mt-100 text-[80px] font-bold">
-                First Tech Coyotes
-            </p>
-            <p className="absolute -ml-[406.61px] -mt-55 text-6xl">Student engineers</p>
+            <div className="absolute flex">
+                <img src="/assets/logo.svg" className=""></img>
+                <div>
+                    <p className="text-[80px] font-bold">First Tech Coyotes</p>
+                    <p className="text-6xl">Student engineers</p>
+                </div>
+            </div>
         </div>
     );
 }
