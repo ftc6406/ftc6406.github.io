f268783c0e8f659fefcd84c508556e576209a9b2
Kevin <kevinbczhang@gmail.com>
2025-07-17 13:48:56 -0400

    Join Us and Awards

--- src/components/contact_us/index.tsx
+++ src/components/contact_us/index.tsx
@@ -1,25 +1,26 @@
-function JoinUs() {
-    return (
-        <footer
-            className="p-8 md:p-16 lg:p-32 space-y-12
-                bg-stone-900 text-base md:text-lg lg:text-xl"
-        >
-            <h1 className="text-3xl md:text-4xl lg:text-5xl" id="join-us">
-                Contact Us
-            </h1>
+import type { ReactNode } from "react";
 
-            <div className="grid lg:grid-flow-col gap-2">
-                <p>Still have any question? Feel free to reach out to us.</p>
-                <p className="select-all">example@gmail.com</p>
-            </div>
+const ContactUs = (): ReactNode => {
+  return (
+    <footer
+      id="contact"
+      className="p-8 md:p-16 lg:p-32 space-y-12
+        bg-stone-900 text-base md:text-lg lg:text-xl"
+    >
+      <h1 className="text-3xl md:text-4xl lg:text-5xl">Contact Us</h1>
 
-            <hr className="text-base text-white leading-0" />
-            <div className="flex items-center space-x-4 ">
-                <img src="/assets/logo.svg" className="w-10 md:w-12 lg:w-14" />
-                <p>First Tech Coyotes</p>
-            </div>
-        </footer>
-    );
+      <div className="grid lg:grid-flow-col lg:grid-cols-2 gap-4 lg:gap-32">
+        <p>Still have any question? Feel free to reach out to us.</p>
+        <p className="select-all">example@gmail.com</p>
+      </div>
+
+      <hr className="text-base text-white/60 leading-0" />
+      <div className="flex items-center space-x-4 ">
+        <img src="/assets/logo.svg" className="w-10 md:w-12 lg:w-14" />
+        <p>First Tech Coyotes</p>
+      </div>
+    </footer>
+  );
 }
 
-export default JoinUs;
+export default ContactUs;
--- src/components/join_us/index.tsx
+++ src/components/join_us/index.tsx
@@ -1,16 +1,75 @@
-function JoinUs() {
-    return (
-        <>
-            <h1 className="pb-4 text-4xl text-center font-bold" id="join-us">
-                Join Us
-            </h1>
+import type { ReactNode } from 'react';
+
+const JoinUs = (): ReactNode => {
+  return (
+    <div id="join">
+      {/* Background*/}
+      <img
+        className="absolute z-1 
+          left-[calc(50%-40px)] md:left-[calc(50%-60px)] lg:left-[calc(50%-80px)] 
+          w-20 md:w-30 lg:w-40"
+        src="/assets/triangle_down.svg"
+      />
+
+      {/* Content */}
+      <div
+        className="p-8 pt-12 md:p-16 lg:p-32 space-y-8 lg:space-y-12
+          bg-white text-base md:text-lg lg:text-xl text-black"
+      >
+        <h1 className="text-3xl md:text-4xl lg:text-5xl">Join Us</h1>
+
+        <div
+          className="grid xl:grid-flow-col xl:grid-cols-2 
+            gap-8 xl:gap-32"
+        >
+          <div>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac
-                magna ex. Donec elementum ultrices ullamcorper. Cras varius
-                ullamcorper mauris, ac eleifend est volutpat vitae.
+              We are aspiring computer scientists and engineers. Our team comes
+              from all levels of technical experience, so we{' '}
+              <span
+                className="relative inline-block 
+                  before:absolute before:inset-0 before:bg-pink-500"
+              >
+                <span className="relative mx-1">
+                  encourage anyone to join, STEM or not,
+                </span>
+              </span>{' '}
+              and are willing to lend our expertise to teach newcomers the
+              skills necessary to explore their passions in robotics and
+              innovation.
             </p>
-        </>
-    );
-}
+          </div>
+
+          <div>
+            <p className="select-all mb-4">
+              The structure of our team is simple. We have three branches, the
+              Engineers, the Programmers, and the Documenters:
+            </p>
+            <ul className="list-disc ml-8">
+              <li>Engineers handle robot assembly and CAD</li>
+              <li>Programmers maintain robot code and logic</li>
+              <li>
+                Documenters write team documentation and the engineering
+                portfolio
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="flex justify-center xs:justify-start">
+          <button
+            className="px-6 py-2 outline-4 outline-primary 
+              text-primary font-medium cursor-pointer
+              transition duration-300
+              hover:bg-primary hover:text-white hover:outline-none
+              active:bg-primary/20 active:text-primary/80 active:outline-none"
+          >
+            Interest Form
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default JoinUs;
