b448cb9ec57e5b8eb422075ede46be6f3c0e9136
Eric Zhang <ericbkzhang@gmail.com>
2025-07-16 22:12:15 -0400

    Adjusted values for small displays

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -2,16 +2,22 @@ function Hero() {
     return (
         <div className="flex justify-center items-center" id="hero">
             <img
-                className="-z-10 w-screen brightness-75"
+                className="-z-10 w-screen h-auto
+                brightness-75"
                 src="/assets/man_mountain.jpg"
             />
 
-            <div className="absolute flex">
-                <img src="/assets/logo.svg" className="" />
+            <div
+                className="absolute -ml-10 md:-ml-200 -mt-10 md:-mt-85 
+                    flex"
+            >
+                <img src="/assets/logo.svg" className="w-[60px] md:w-[178px]" />
 
-                <div className="">
-                    <p className="text-[80px] font-bold">First Tech Coyotes</p>
-                    <p className="text-6xl">Student engineers</p>
+                <div className="ml-2 md:ml-12 mt-2 md:mt-6">
+                    <p className="text-2xl md:text-[80px] font-bold">
+                        First Tech Coyotes
+                    </p>
+                    <p className="text-xl md:text-6xl">Student engineers</p>
                 </div>
             </div>
         </div>
