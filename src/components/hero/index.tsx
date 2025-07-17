3589d54943ab83d5260ae00d53559b2033537629
Eric Zhang <ericbkzhang@gmail.com>
2025-07-17 17:59:12 -0400

    Update responsive design.
    Make the chevron absoluate, increase chevron's size and distance from bottom at small sizes.

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,44 +1,41 @@
 function Hero() {
   return (
-    <div className="bg-[url(assets/man_mountain.jpg)] bg-center" id="hero">
+    <div
+      className="relative bg-[url(assets/man_mountain.jpg)] bg-center"
+      id="hero"
+    >
       {/* Backdrop filter */}
       <div
-        className="h-[100vh] px-8 md:px-16 lg:px-32 py-24 md:py-36 lg:py-44 space-y-12
-          place-items-center lg:place-items-start
+        className="h-[100vh] px-8 md:px-16 lg:px-24 xl:px-28 2xl:px-32 sm:py-56 md:py-52 lg:py-48 2xl:py-44 space-y-12
+          place-items-center xl:place-items-start
           backdrop-brightness-70"
       >
         {/* Content */}
         <div
-          className="flex flex-col lg:flex-row gap-0 md:gap-10
-            place-items-center lg:place-items-start"
+          className="flex flex-col xl:flex-row gap-0 md:gap-10
+            place-items-center xl:place-items-start"
         >
           {/* Logo */}
           <div className="shrink-0">
             <img
               src="/assets/logo.svg"
-              className="w-24 md:w-32 lg:40 xl:w-44"
+              className="w-28 md:w-32 lg:w-36 xl:w-40 2xl:w-44"
             />
           </div>
 
           {/* Text */}
           <div
-            className="mt-8 grid lg:grid-cols-2 gap-8 md:gap-12 lg:gap-18 xl:gap-24
-              place-items-center lg:place-items-start"
+            className="mt-4 md:mt-5 lg:mt-6 grid xl:grid-cols-2 gap-12 lg:gap-16 xl:gap-20 2xl:gap-24
+              place-items-center xl:place-items-start"
           >
             {/* Team Name */}
             <div>
-              <div className="space-y-2 md:space-y-4 text-center lg:text-left">
-                <h1
-                  className=" 
-                    text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold"
-                >
+              <div className="space-y-4 text-center xl:text-left">
+                <h1 className="text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold">
                   First Tech Coyotes
                 </h1>
 
-                <h2
-                  className="
-                    text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl"
-                >
+                <h2 className="text-4xl 2xl:text-5xl">
                   Student engineers
                 </h2>
               </div>
@@ -47,9 +44,9 @@ function Hero() {
             {/* Button */}
             <div className="col-start-1">
               <button
-                className="px-6 md:px-8 lg:px-12 xl:px-28 py-4
+                className="hidden xl:block px-6 md:px-12 lg:px-16 xl:px-24 2xl:px-28 py-4
                   outline-5 outline-white
-                  text-xl md:text-2xl lg:text-3xl font-bold"
+                  xl:text-2xl 2xl:text-3xl font-bold"
               >
                 Join Us
               </button>
@@ -57,7 +54,7 @@ function Hero() {
 
             {/* Team description */}
             <div>
-              <p className="hidden lg:block text-right lg:text-2xl 2xl:text-3xl">
+              <p className="hidden xl:block text-right xl:text-2xl 2xl:text-3xl">
                 We are the First Tech Coyotes, a large public school team that
                 participates in the FIRST Tech Challenge. As a diverse body of
                 students pursuing STEM, we seek to inspire and teach like-minded
@@ -69,7 +66,7 @@ function Hero() {
 
         <img
           src="assets/chevron.svg"
-          className="absolute place-self-center w-12 md:w-20 lg:w-28 animate-bounce"
+          className="absolute bottom-28 md:bottom-24 lg:bottom-12 place-self-center w-36 md:w-32 lg:w-28 animate-bounce"
         />
       </div>
     </div>
