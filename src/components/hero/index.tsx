6ec14086d42a1837cbf345dfe2cae65280e5fe9a
Eric Zhang <ericbkzhang@gmail.com>
2025-07-17 00:41:29 -0400

    Turn hero content into a grid
    Yay?

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,23 +1,47 @@
 function Hero() {
     return (
-        <div className="flex justify-center items-center" id="hero">
-            <img
-                className="-z-10 w-screen h-auto
-                brightness-75"
-                src="/assets/man_mountain.jpg"
-            />
-
+        <div className="bg-[url(assets/man_mountain.jpg)] bg-center" id="hero">
+            {/* Backdrop filter */}
             <div
-                className="absolute -ml-10 md:-ml-200 -mt-10 md:-mt-85 
-                    flex"
+                className="h-[100vh] pt-64
+                    flex flex-row
+                    backdrop-brightness-70"
             >
-                <img src="/assets/logo.svg" className="w-[60px] md:w-[178px]" />
+                {/* Content */}
+                <div className="grid-cols-subgrid">
+                    {/* Logo */}
+                    <div className="pl-32">
+                        <img src="/assets/logo.svg" className="w-44" />
+                    </div>
+                    {/* Text */}
+                    <div className="pt-8 pl-12">
+                        <h1
+                            className="text-2xl md:text-4xl lg:text-6xl 
+                            font-bold"
+                        >
+                            First Tech Coyotes
+                        </h1>
 
-                <div className="ml-2 md:ml-12 mt-2 md:mt-6">
-                    <p className="text-2xl md:text-[80px] font-bold">
-                        First Tech Coyotes
-                    </p>
-                    <p className="text-xl md:text-6xl">Student engineers</p>
+                        <h2 className="text-5xl">Student engineers</h2>
+                        {/* <div className="flex flex-row">
+                    <button
+                        type="button"
+                        className="w-[313px] h-[77px]
+                                border-5 text-[32px]"
+                    >
+                        Join Us
+                    </button>
+
+                    <div>
+                        <p className="text-[32px] w-[721px]">
+                            We are the First Tech Coyotes, a large public school
+                            team that participates in the FIRST Tech Challenge.
+                            As a diverse body of students pursuing STEM, we seek
+                            to inspire and teach like-minded youth in robotics.
+                        </p>
+                    </div>
+                </div> */}
+                    </div>
                 </div>
             </div>
         </div>
