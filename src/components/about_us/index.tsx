c944e53cb441f3b2fc82021ddef0b3fe7258e6c1
Eric Zhang <ericbkzhang@gmail.com>
2025-07-18 21:15:12 -0400

    Add a About Us section for large screens and below

--- src/components/about_us/index.tsx
+++ src/components/about_us/index.tsx
@@ -4,10 +4,13 @@ import AboutUsText from './aboutUsText';
 const AboutUs = (): ReactNode => {
   return (
     <div
-      className="block xl:hidden bg-white
-      text-center"
+      className="block xl:hidden
+      flex justify-center items-center 
+      h-100
+      bg-white
+      text-center text-3xl"
     >
-      <p className="text-black">
+      <p className="w-2xl text-black">
         <AboutUsText />
       </p>
     </div>
