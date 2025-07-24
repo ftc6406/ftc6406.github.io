aa55dbe8a61a55eb60faf9b5e771e3c1ef76c10c
Eric Zhang <ericbkzhang@gmai.com>
2025-07-24 14:58:11 -0400

    Apply top margin to right-most column

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -11,14 +11,14 @@ const Gallery = (): ReactNode => {
 
   let num_cols;
   if (window.innerWidth > 1024) {
-    num_cols = 3;
+    num_cols = 6;
   } else if (window.innerWidth > 768) {
-    num_cols = 2;
+    num_cols = 4;
   } else {
-    num_cols = 1;
+    num_cols = 2;
   }
 
-  // Separte the images into 3 columns.
+  // Separte the images into columns.
   const cols: string[][] = [];
   for (let i = 0; i < num_cols; i++) {
     cols.push(imagePaths.filter((_, index) => index % num_cols === i));
@@ -28,7 +28,7 @@ const Gallery = (): ReactNode => {
     <div
       id="gallery"
       className="w-full
-        px-20 md:px-24 lg:px-28 py-28 md:py-32 lg:py-36
+        px-24 md:px-28 lg:px-32 py-28 md:py-32 lg:py-36
         bg-linear-to-r from-accent to-primary"
     >
       <h1
@@ -41,13 +41,17 @@ const Gallery = (): ReactNode => {
       {/* Gallery columns */}
       <div
         id="gallery-images"
-        className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-end`}
+        className={`grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 justify-end`}
       >
         {/* Create each column */}
         {cols.map((col, colIndex) => {
           return (
             // Place images in each column.
-            <div key={`gallery-col-${colIndex}`} className="grid gap-4">
+            <div
+              key={`gallery-col-${colIndex}`}
+              className={`mt-${colIndex * 10}
+                grid gap-4`}
+            >
               {col.map((imagePath, imageIndex) => {
                 {
                   return (
