22ff5f0f06bed1aefc6af8f111c5734b49fc618f
Eric Zhang <ericbkzhang@gmai.com>
2025-07-24 19:56:58 -0400

    Add basic carousel

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -1,4 +1,6 @@
 import type { ReactNode } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 // Retrieves all files in `assets/gallery`.
 const modules = import.meta.glob('/public/assets/gallery/*');
@@ -7,15 +9,30 @@ const Gallery = (): ReactNode => {
   // Extract the filepaths.
   const imagePaths = Object.keys(modules);
 
-  console.log(window.innerWidth);
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 1280, min: 1024 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 768, min: 640 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 640, min: 0 },
+      items: 1,
+    },
+  };
 
   let num_cols;
   if (window.innerWidth > 1024) {
     num_cols = 6;
-  } else if (window.innerWidth > 768) {
-    num_cols = 4;
   } else {
-    num_cols = 2;
+    num_cols = 4;
   }
 
   // Separte the images into columns.
@@ -38,10 +55,10 @@ const Gallery = (): ReactNode => {
         Gallery
       </h1>
 
-      {/* Gallery columns */}
+      {/* Gallery masonry */}
       <div
-        id="gallery-images"
-        className={`grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 justify-end`}
+        id="gallery-masonry"
+        className="hidden md:grid md:grid-cols-4 lg:grid-cols-6 gap-4"
       >
         {/* Create each column */}
         {cols.map((col, colIndex) => {
@@ -66,6 +83,15 @@ const Gallery = (): ReactNode => {
             </div>
           );
         })}
+      </div>
+
+      {/* Gallery carousel */}
+      <div id="gallery-carousel" className="block md:hidden">
+        <Carousel responsive={responsive}>
+          {imagePaths.map((image) => {
+            return <img src={image} />;
+          })}
+        </Carousel>
       </div>
     </div>
   );
