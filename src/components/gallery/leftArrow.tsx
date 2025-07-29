01a1f2d35dac36c4c0190cee224dd97a6eb05932
Eric Zhang <ericbkzhang@gmail.com>
2025-07-28 20:24:55 -0400

    Add left and right custom arrows for Gallery carousel

---
+++ src/components/gallery/leftArrow.tsx
@@ -0,0 +1,10 @@
+const LeftArrow = ({ onClick, ...rest }) => {
+  const {
+    onMove,
+    carouselState: { currentSlide, deviceType },
+  } = rest;
+  // onMove means if dragging or swiping in progress.
+  return <button onClick={() => onClick()} />;
+};
+
+export default LeftArrow;
---
+++ src/components/gallery/rightArrow.tsx
@@ -0,0 +1,10 @@
+const RightArrow = ({ onClick, ...rest }) => {
+  const {
+    onMove,
+    carouselState: { currentSlide, deviceType },
+  } = rest;
+  // onMove means if dragging or swiping in progress.
+  return <button onClick={() => onClick()} />;
+};
+
+export default RightArrow;
