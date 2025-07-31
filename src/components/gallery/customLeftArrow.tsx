3b36ff0481854473ca4ccb0ef4ee00e088607db8
Eric Zhang <ericbkzhang@gmail.com>
2025-07-31 12:49:48 -0400

    Resolve a problem with `onClick()` possibly being null in custom arrows

--- src/components/gallery/customLeftArrow.tsx
+++ src/components/gallery/customLeftArrow.tsx
@@ -1,6 +1,14 @@
 const CustomLeftArrow = ({ onClick }: { onClick?: () => void }) => {
   return (
-    <button onClick={() => onClick()}>
+    <button
+      onClick={() => {
+        if (onClick != null) {
+          onClick();
+        } else {
+          console.log(`${onClick} is null`);
+        }
+      }}
+    >
       <img src="/assets/leftArrow.svg" />
     </button>
   );
--- src/components/gallery/customRightArrow.tsx
+++ src/components/gallery/customRightArrow.tsx
@@ -1,6 +1,14 @@
-const CustomRightArrow = ({ onClick }: {onClick?: () => void}) => {
+const CustomRightArrow = ({ onClick }: { onClick?: () => void }) => {
   return (
-    <button onClick={() => onClick()}>
+    <button
+      onClick={() => {
+        if (onClick != null) {
+          onClick();
+        } else {
+          console.log(`${onClick} is null`);
+        }
+      }}
+    >
       <img src="/assets/rightArrow.svg" />
     </button>
   );
