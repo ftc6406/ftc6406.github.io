3b17e0648729acc703d104d6dd95c11b46ddca1a
Eric Zhang <ericbkzhang@gmail.com>
2025-07-31 17:22:47 -0400

    Set carousel images to have same height

--- src/components/gallery/card.tsx
+++ src/components/gallery/card.tsx
@@ -12,19 +12,15 @@ const Card = ({
   background?: boolean;
 }): ReactNode => {
   return (
-    <div
-      className={`relative flex ${background ? 'flex-col' : ''} size-full object-cover`}
-    >
-      <img src={imagePath} />
+    <div className={`relative flex ${background ? 'flex-col' : ''}`}>
+      <img src={imagePath} className="aspect-3/2 object-cover" />
 
       <div
         className={`${
           background ? 'bg-stone-900' : 'absolute self-end'
         } px-4 py-8`}
       >
-        <p className="body-text opacity-80 font-bold">
-          {title}
-        </p>
+        <p className="body-text opacity-80 font-bold">{title}</p>
         <p className="text-xl md:text-2xl lg:text-3xl">{body}</p>
       </div>
     </div>
