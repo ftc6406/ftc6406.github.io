797de77e9f6a32d77668282b7cb26a083513ac41
Eric Zhang <ericbkzhang@gmail.com>
2025-08-02 18:22:31 -0400

    Temporarily remove Gallery placeholders

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -9,9 +9,9 @@ import Card from './card';
 // Retrieves all files in `assets/gallery`.
 const MODULES = import.meta.glob('/public/assets/gallery/*');
 // Extract the filepaths.
-const IMAGE_PATHS = Object.keys(MODULES).map((ele) =>
-  ele.replace('/public', '')
-);
+const IMAGE_PATHS = Object.keys(MODULES)
+  .filter((ele) => !ele.includes('placeholder'))
+  .map((ele) => ele.replace('/public', ''));
 
 const RESPONSIVE = {
   superLargeDesktop: {
@@ -63,9 +63,9 @@ const Gallery = (): ReactNode => {
     );
   });
 
-  for (let i = 0; i < 2; i++) {
-    cards.push(...cards);
-  }
+  // for (let i = 0; i < 2; i++) {
+  //   cards.push(...cards);
+  // }
 
   return (
     <div
