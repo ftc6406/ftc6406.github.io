8bfcc943c3af205717bcd3120ac209e39437dddb
Eric Zhang <ericbkzhang@gmail.com>
2025-08-03 09:10:26 -0400

    Adjust line height of Gallery images

--- src/components/gallery/card.tsx
+++ src/components/gallery/card.tsx
@@ -21,7 +21,7 @@ const Card = ({
         } px-4 md:px-8 lg:px-4 py-6 md:py-12 lg:py-4 xl:py-6 2xl:py-8`}
       >
         <p className="opacity-80 text-lg md:text-2xl lg:text-base xl:text-lg 2xl:text-xl font-bold">{title}</p>
-        <p className="text-xl md:text-4xl lg:text-xl xl:text-2xl 2xl:text-3xl">{body}</p>
+        <p className="text-2xl/8 md:text-4xl/12 lg:text-xl/6 xl:text-2xl/7 2xl:text-3xl/8">{body}</p>
       </div>
     </div>
   );
